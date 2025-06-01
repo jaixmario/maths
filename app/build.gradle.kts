ab68529be9431dc7eea8dd8b070ef84d4a882493
Jaideep <jaideepindia99@gmail.com>
2025-06-02 03:12:56 +0530

    welcome screen added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jai.mario"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.3"
+        versionCode = 4
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
