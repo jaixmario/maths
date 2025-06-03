e5ae52eb319c03501f5d968fed36daec0da2d6da
Jaideep <jaideepindia99@gmail.com>
2025-06-04 02:59:35 +0530

    Update:tesing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jai.mario"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.1.8"
+        versionCode = 9
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
