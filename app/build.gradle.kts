c5f190222543753ff07ccfaac0776b87f42149eb
Jaideep <jaideepindia99@gmail.com>
2025-06-04 03:00:27 +0530

    Update:update checker added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 9
-        versionName = "1.1.91"
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
