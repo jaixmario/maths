376c1675b6c21c6f461481437265d42829c38cab
Jaideep <jaideepindia99@gmail.com>
2025-06-04 02:09:50 +0530

    Update:fix for navigation & size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jai.mario"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.7"
+        versionCode = 8
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
