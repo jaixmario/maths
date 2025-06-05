5138e562bd2ae091289460b9408de8d4e6d719e3
Jaideep <jaideepindia99@gmail.com>
2025-06-05 16:08:22 +0530

    Update:gg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,53 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.jai.mario"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jai.mario"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 10
+        versionName = "1.1.10"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_PATH"))
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias    = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
+    buildTypes {
+        debug {
+        }
+        release {
+            signingConfig = signingConfigs.getByName("release")
+
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.squareup.okio:okio:3.8.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("com.google.android.material:material:1.14.0-alpha01")
 }
